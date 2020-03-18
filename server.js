f2becc0ce0ef4dd8986d42d0c9a4f146f9a2671b
Truong <nguyentientruong95@gmail.com>
2020-03-18 17:48:33 +0900

    change mode router

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname + "/dist/"));
-app.listen(port);
+var express = require('express');
+var app = express();
+var history = require('connect-history-api-fallback');
+
+app.use(history({
+  // verbose: true
+}));
+
+app.use(express.static(__dirname + '/dist'));
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), () => {
+  console.log(`ENV: ${process.env.NODE_ENV}`);
+  console.log(`Derp is running at localhost: ${app.get('port')}`);
+});
